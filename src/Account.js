48eee43d28c711516a4e8116e4f2658c478243a9
Amy Leung <leungmya@gmail.com>
2024-05-02 17:23:19 +0100

    chore: refactor transaction code into one method

--- src/Account.js
+++ src/Account.js
@@ -21,24 +21,12 @@ export default class Account {
 
     deposit = (amount, date) => {
         const tDate = dateReformatter(date);
-        const transaction = {
-            "date": tDate,
-            "type": "credit",
-            "amount": amount.toFixed(2),
-            "balance": (this.#balance += amount).toFixed(2),
-        };
-        this.#statement.unshift(transaction);
+        this.handleStatement(tDate, "credit", amount, (this.#balance += amount));
         return true;
     };
 
     withdraw = (amount, date) => { 
         const tDate = dateReformatter(date);
-        const transaction = {
-            "type": "debit",
-            "amount": amount.toFixed(2),
-            "date": tDate,
-            "balance": this.#balance.toFixed(2),
-        };
 
         if (this.#overdraft) {
             this.handleOverdraft(amount, tDate);
@@ -48,15 +36,14 @@ export default class Account {
         if (amount <= this.#balance) {
             //? If value is less than or equal to current account balance, deduct value
             this.#balance -= amount;
-            this.#statement.unshift(transaction);
+            this.handleStatement(tDate, "debit", amount);
             return true;
         } else { 
             //? Insufficient funds, only withdraw up until balance reaches 0
             const bal = this.#balance - amount;
             const withdrawn = amount - Math.abs(bal);
             this.#balance -= withdrawn;
-            transaction["amount"] = withdrawn;
-            this.#statement.unshift(transaction);
+            this.handleStatement(tDate, "debit", withdrawn);
             return false; 
         }
     }
@@ -80,27 +67,30 @@ export default class Account {
     }
 
     handleOverdraft = (amount, date) => { 
-        const transaction = {
-            "type": "debit",
-            "amount": amount.toFixed(2),
-            "date": date,
-            "balance": this.#balance.toFixed(2),
-        };
         const availableBalance = this.#balance + this.#overdraftLimit;
         
         if (amount <= availableBalance) {
             //? Withdraw money
             this.#balance -= amount;
-            this.#statement.unshift(transaction);
+            this.handleStatement(date, "debit", amount);
             return true;
         } else { 
             //? Insufficient funds, withdraw up till overdraft limit
             const withdrawn = this.#balance + this.#overdraftLimit;
 
             this.#balance -= withdrawn;
-            transaction["amount"] = withdrawn;
-            this.#statement.unshift(transaction);
+            this.handleStatement(date, "debit", withdrawn);
             return false;
         }
+    }
+
+    handleStatement = (date, type, amount, balance) => { 
+        const transaction = {
+            "date": date,
+            "type": type,
+            "amount": amount.toFixed(2),
+            "balance": balance ? balance.toFixed(2) : this.#balance,
+        };
+        this.#statement.unshift(transaction);
     }
 }
